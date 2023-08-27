3dd1a9c87d668cfe80198f95f1c65e2301270b17
prafitradimas <prftrdms@gmail.com>
2023-08-28 01:37:03 +0700

    feat: enable service discovery

--- eureka-service-discovery/src/main/java/com/github/prafitradimas/service/discovery/EurekaServiceDiscoveryApplication.java
+++ eureka-service-discovery/src/main/java/com/github/prafitradimas/service/discovery/EurekaServiceDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.github.prafitradimas.service.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.*;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
