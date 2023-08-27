28e8e59a4196460d733c81cc13e4f30dfd30ad96
prafitradimas <prftrdms@gmail.com>
2023-08-28 01:13:49 +0700

    feat: initial commit for eureka server

---
+++ eureka-service-discovery/src/main/java/com/github/prafitradimas/service/discovery/EurekaServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.github.prafitradimas.service.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ eureka-service-discovery/src/test/java/com/github/prafitradimas/service/discovery/EurekaServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.prafitradimas.service.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
