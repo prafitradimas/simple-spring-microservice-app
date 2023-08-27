b7644049387e1e29029990ab52b0c0ac29399342
prafitradimas <prftrdms@gmail.com>
2023-08-28 02:58:23 +0700

    feat: add config server

---
+++ config-server/src/main/java/com/github/prafitradimas/config/ConfigurationServerApplication.java
@@ -0,0 +1,17 @@
+package com.github.prafitradimas.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.*;
+import org.springframework.cloud.config.server.*;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServerApplication.class, args);
+	}
+
+}
